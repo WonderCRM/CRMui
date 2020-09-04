175caf805e2bdd113e58b18733580524035442fa
WonderCRM <46189077+WonderCRM@users.noreply.github.com>
2020-09-04 23:16:39 +0300

    Update config.cpp

--- CRMui/config.cpp
+++ CRMui/config.cpp
@@ -4,7 +4,11 @@
 void CRMui::save_cfg() {
   String pre_config;
   serializeJson(cfg, pre_config);
+#ifdef ESP8266
   SPIFFS.open(F("/config.json"), "w").print(pre_config);
+#else
+  SPIFFS.open(F("/config.json"), FILE_WRITE).print(pre_config);
+#endif
   Serial.println(F("Конфиг сохранен."));
 }
 
@@ -18,6 +22,7 @@ void CRMui::autosave() {
 
 
 void CRMui::load_cfg() {
+#ifdef ESP8266
   String pre_config = SPIFFS.open(F("/config.json"), "r").readString();
   if (pre_config == "") {
     Serial.println(F("Конфиг пуст, настройки сброшены по умолчанию."));
@@ -26,6 +31,17 @@ void CRMui::load_cfg() {
     deserializeJson(cfg, pre_config);
     Serial.println(F("Конфиг загружен."));
   }
+#else
+  File file = SPIFFS.open(F("/config.json"));
+  if (!file || file.isDirectory()) {
+    Serial.println(F("Конфиг пуст, настройки сброшены по умолчанию."));
+    save_cfg();
+  } else {
+    deserializeJson(cfg, file);
+    file.close();
+    Serial.println(F("Конфиг загружен."));
+  }
+#endif
 }
 
 
