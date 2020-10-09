54ece6282d3fcf312d66b58ac84e54579930c27d
WonderCRM <46189077+WonderCRM@users.noreply.github.com>
2020-10-09 14:34:19 +0300

    Update NewProject.ino

--- CRMui/examples/NewProject/NewProject.ino
+++ CRMui/examples/NewProject/NewProject.ino
@@ -1,5 +1,6 @@
-#include "CRMui.h"
-#define VER "___"
+#include "aliases.h"
+#include <CRMui.h>
+#define VER F("___")
 
 #define LED 2
 
@@ -7,12 +8,13 @@
 CRMui crm;
 
 
+
 void setup() {
-  Serial.begin(115200);
   var();
   update_vol();
-
+  crm.setWebAut(crm.param(WEB_AUTH_LOGIN), crm.param(WEB_AUTH_PASS));    //Авторизация в Web интерфейсе
 }
+
 
 
 void update_vol() {
@@ -20,14 +22,24 @@ void update_vol() {
 }
 
 
+
 void loop() {
   crm.handle();
-  crm.btnCallback(F("RBT"), Reboot_ESP);
+  crm.btnCallback(BT_REBOOT, ESP_reboot);
+  crm.btnCallback(BT_RESET, ESP_reset);
 
 }
 
 
+
 // Перезагрузка ESP
-void Reboot_ESP() {
+void ESP_reboot() {
   crm.reboot();
+}
+
+
+
+// Сброс настроек ESP
+void ESP_reset() {
+  crm.reset_cfg();
 }
